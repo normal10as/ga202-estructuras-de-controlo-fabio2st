52795c41ea81e5492b28aa8532a08e0a3ed169d2
Fabio Dos Santos <fabio2st@yahoo.com>
2022-10-03 22:53:46 -0300

    Primos

---
+++ GA202-estructuras-de-controlo-fabio2st/PrimosCs/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Un número primo es un número natural mayor que 1 que tiene únicamente dos divisores distintos: él mismo y el 1");
+uint divisor;
+double raiz;
+
+for (ushort x = 2; x <= 1000; x++)
+{
+    raiz = Math.Sqrt(x);
+    divisor = 2;
+    while (x % divisor != 0 && divisor <= raiz)
+        divisor++;
+    if (x % divisor != 0 || divisor > raiz)
+        Console.Write(" " + x);
+}

fef49b5b0824af445887c66bbcd93b19479fc4e0
Fabio Dos Santos <fabio2st@yahoo.com>
2022-10-13 20:19:43 -0300

    2.20.Deposito

---
+++ GA202-estructuras-de-controlo-fabio2st/2.20.DepositoCs/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DepositoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal capitalInicial, interesAnual, interesPesos;
+            Console.Write("Ingrese Capital: ");
+            capitalInicial = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Ingrese Interés anual: ");
+            interesAnual = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("MES  CAPITAL $  INTERES $       SALDO $");
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(1, capitalInicial, interesPesos);
+            capitalInicial += interesPesos;
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(2, capitalInicial, interesPesos);
+            capitalInicial += interesPesos;
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(3, capitalInicial, interesPesos);
+            capitalInicial += interesPesos;
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(4, capitalInicial, interesPesos);
+            capitalInicial += interesPesos;
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(5, capitalInicial, interesPesos);
+            capitalInicial += interesPesos;
+            interesPesos = CalcularInteresEnPesos(capitalInicial, interesAnual);
+            Reporte(6, capitalInicial, interesPesos);
+        }
+        static decimal CalcularInteresEnPesos(decimal capital, decimal interesAnual)
+        {
+            return capital * interesAnual / 12 / 100;
+        }
+        static void Reporte(ushort mes, decimal capital, decimal interesPesos)
+        {
+            Console.WriteLine($"{mes}    {capital:N2}     {interesPesos:N2}     {capital+interesPesos:N2}");
+        }
+    }
+}

121703caaa30287c01de01eb01acfaa0419a1f87
Fabio Dos Santos <fabio2st@yahoo.com>
2022-10-13 19:54:57 -0300

    2.01.Hipotenusa

---
+++ GA202-estructuras-de-controlo-fabio2st/2.01.HipotenusaCs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HipotenusaCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ladoA,ladoB;
+            Console.Write("Ingrese el lado A: ");
+            ladoA = Convert.ToDouble( Console.ReadLine());
+            Console.Write("Ingrese el lado B: ");
+            ladoB  = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Hipotenusa: " + CalcularHipotenusa(ladoA, ladoB));
+        }
+        static double CalcularHipotenusa(double ladoA , double ladoB)
+        {
+            var ladoC = Math.Sqrt( Math.Pow(ladoA, 2) + Math.Pow(ladoB, 2));
+            return ladoC;
+        }
+    }
+}

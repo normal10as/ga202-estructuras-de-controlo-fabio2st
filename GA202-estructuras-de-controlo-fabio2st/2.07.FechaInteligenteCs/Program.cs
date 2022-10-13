02f707584f6818d898404d8b3c161cb840b3f7e9
Fabio Dos Santos <fabio2st@yahoo.com>
2022-10-13 20:16:51 -0300

    2.07.FechaInteligente

---
+++ GA202-estructuras-de-controlo-fabio2st/2.07.FechaInteligenteCs/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HipotenusaCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ObtenerFecha(20));
+            Console.WriteLine(ObtenerFecha(20, 6));
+            Console.WriteLine( ObtenerFecha(20,6,2023));
+        }
+
+        private static DateTime ObtenerFecha(int day, int month, int year)
+        {
+            return new DateTime(year, month, day);
+        }
+        static DateTime ObtenerFecha(int day, int month)
+        {
+            return new DateTime(DateTime.Now.Year, month, day);
+        }
+        static DateTime ObtenerFecha(int day)
+        {
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, day);
+        }
+    }
+}

089b50d7dd80450156a205e3429d7df0a56f97ac
Fabio Dos Santos <fabio2st@yahoo.com>
2022-10-13 20:06:35 -0300

    2.03.AhoraEs

---
+++ GA202-estructuras-de-controlo-fabio2st/2.03.AhoraEsCs/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateNow();
+TimeSpan dateNow = TimeNow();
+Console.WriteLine("Hora: " + dateNow.ToString(@"hh\:mm"));
+
+static void DateNow()
+{
+    Console.WriteLine("Fecha: " + DateTime.Now.Date.ToShortDateString());
+}
+static TimeSpan TimeNow()
+{
+    return DateTime.Now.TimeOfDay;
+}
